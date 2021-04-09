d482fefb45269282648a5bc7ad0672e5b63423da
Daniel Jin Yamamoto <jinebas@hotmail.com>
2021-04-09 17:20:53 -0300

    Update seller and custom service exception

---
+++ SalesWebMvc/Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SalesWebMvc.Services.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(String message) : base(message)
+        {
+        }
+
+
+    }
+}
